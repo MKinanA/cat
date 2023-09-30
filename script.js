470fe5d4055e3c03ea2df50770fb27c80da3eb63
MKinanA <mkinanahsan@gmail.com>
2023-09-30 08:31:38 +0700

    changes 30092023

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let menubtn = document.querySelector('.bx-menu');
 let nav = document.querySelector('nav');
+let body = document.querySelector('body');
 menubtn.addEventListener('click', function() {
     nav.classList.toggle('active');
     menubtn.classList.toggle("bx-x");
+    body.classList.toggle('inactive');
 });
 nav.addEventListener('click', function() {
     nav.classList.remove('active');
     menubtn.classList.remove("bx-x");
+    body.classList.remove('inactive');
 });
